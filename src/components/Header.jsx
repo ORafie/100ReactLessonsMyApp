2bd095512ab5384a8c86ce7ddb46af2bbe36db45
Olena Rafieienko <rafeyenko@yahoo.com>
2023-03-09 20:29:10 +0100

    divided into components

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0ntbd58-RDIiZicZqpuY94FchXYUOwYLnjQ&usqp=CAU' />
+    </header>
+}
+export default Header;
