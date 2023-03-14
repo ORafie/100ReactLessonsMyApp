04cf759de82ddbcff7c57e59ce90eb7a6364cfad
rafeyenko@yahoo.com <rafeyenko@yahoo.com>
2023-03-14 17:53:38 +0100

    14.css_modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
+import classes from './Header.module.css'
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={classes.header}>
         <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0ntbd58-RDIiZicZqpuY94FchXYUOwYLnjQ&usqp=CAU' />
     </header>
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+import classes from './Navbar.module.css'
+console.log(classes);
 
 const Navbar = () => {
-    return <nav className='nav'>
-        <div><a>Profile</a></div>
-        <div><a>Messages</a></div>
-        <div><a>New</a>s</div>
-        <div><a>Music</a></div>
-        <div><a>Settings</a></div>
+  return <nav className={classes.nav}>
+    <div className={classes.item}><a>Profile</a></div>
+        <div className={`${classes.item} ${classes.active}`}><a>Messages</a></div>
+        <div className={classes.item}><a>New</a>s</div>
+        <div className={classes.item}><a>Music</a></div>
+        <div className={classes.item}><a>Settings</a></div>
       </nav>
 }
 export default Navbar;
