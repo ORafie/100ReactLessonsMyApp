f0a67e29dcd46436e43a5b9826eea734a2312ba6
rafeyenko@yahoo.com <rafeyenko@yahoo.com>
2023-03-19 16:49:45 +0100

    16_likes_counter_easiest

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,32 +7,18 @@ const Post = (props) => {
         <div className={classes.item}>
             <img src='https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569__340.jpg' />
             {props.message}
-            <div className='counter'>
-                <button className='counter__btn' data-direction='plus'>like</button>
-                <input type='text' value='0' className='counter__value' />
+            <div>
+                <button className='counter'>like</button>
             </div>
         </div>
     )
 }
 
-const btns = document.querySelectorAll('.counter__btn');
+const button = document.querySelectorAll('.counter');
+let count;
 
-btns.forEach(btn => {
-    btn.addEventListener( 'click',
-        function () {
-            const direction = this.dataset.direction;
-            const inp = this.parentElement
-                .querySelector('.counter__value');
-            const currentValue = +inp.value;
-            let newValue;
-            if (direction === 'plus') {
-                newValue = currentValue + 1;
-            } else {
-
-            }
-            inp.value = newValue;
-    })
-})
-
+button.addEventListener('click', () => {
+    count = + 1;
+});
 
 export default Post;
