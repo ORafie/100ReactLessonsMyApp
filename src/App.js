52fc2744826a661fd00476f2d94ab1946af2874c
rafeyenko@yahoo.com <rafeyenko@yahoo.com>
2023-03-21 15:04:10 +0100

    19. browser router, routes, route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,29 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Messages from './components/Messages/Messages';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-        {/* <Profile /> */}
-        <Messages />
-      </div>
-    </div >
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path='/news' element={<News />} />
+            <Route path='/music' element={<Music />} />
+            <Route path='/settings' element={<Settings />} />
+          </Routes>
+        </div>
+      </div >
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,11 +3,16 @@ console.log(classes);
 
 const Navbar = () => {
   return <nav className={classes.nav}>
-    <div className={classes.item}><a>Profile</a></div>
-        <div className={`${classes.item} ${classes.active}`}><a>Messages</a></div>
-        <div className={classes.item}><a>New</a>s</div>
-        <div className={classes.item}><a>Music</a></div>
-        <div className={classes.item}><a>Settings</a></div>
-      </nav>
+    <div className={classes.item}>
+        <a href='/profile'>Profile</a></div>
+    <div className={`${classes.item} ${classes.active}`}>
+        <a href='/messages'>Messages</a></div>
+    <div className={classes.item}>
+      <a href='/news'>News</a></div>
+    <div className={classes.item}>
+      <a href='/music'>Music</a></div>
+    <div className={classes.item}>
+      <a href='/settings'>Settings</a></div>
+  </nav>
 }
 export default Navbar;
