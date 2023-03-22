7401bf6274e5263a3903eef0f4319d27fd888bf1
rafeyenko@yahoo.com <rafeyenko@yahoo.com>
2023-03-22 16:19:22 +0100

    23.props for Messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,38 +1,37 @@
 import classes from './Messages.module.css';
+import { NavLink } from 'react-router-dom';
 
-const users = () => {
+const User = (props) => {
+    let path = '/messages/' + props.id;
+
+    return <div className={classes.user}>
+        <NavLink to={path} className={({ isActive }) => isActive ? classes.active : undefined} >{props.name}</NavLink>
+        <img src='https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569__340.jpg' />
+                </div>
+}
+
+const Text = (props) => {
+    return <div className={classes.text}>{props.text}</div>
+}
+
+const Messages = () => {
     return (
         <div className={classes.messages}>
             <div className={classes.users}> 
-                <div className={classes.user + " " + classes.active}>
-                    User1
-                    <img src='https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569__340.jpg' />
-                </div>
-                <div className={classes.user}>
-                    User2
-                    <img src='https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569__340.jpg' />
-                </div>
-                <div className={classes.user}>
-                    User3
-                 <img src='https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569__340.jpg' />
-                </div>
-                <div className={classes.user}>
-                    User4
-                 <img src='https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569__340.jpg' />
-                </div>
-                <div className={classes.user}>
-                    User5
-                 <img src='https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569__340.jpg' />
-                </div>
+                <User name='User 1' id='1' />
+                <User name='User 2' id='2' />
+                <User name='User 3' id='3' />
+                <User name='User 4' id='4' />
+                <User name='User 5' id='5' />
             </div>
-            <div className={classes.texts} >
-                <div className={classes.text}>Hello!</div>
-                <div className={classes.text}>Where have you been!</div>
-                <div className={classes.text}>Glad to see you again!</div>
+            <div className={classes.texts}>
+                <Text text='Hello world!' />
+                <Text text='Where have you been!' />
+                <Text text='Glad to see you again!' />
             </div>
         </div>
             
     )
 }
 
-export default users;
+export default Messages;
