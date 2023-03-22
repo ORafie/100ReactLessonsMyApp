c6e5315222ca257edb908a88c871164a34f31427
rafeyenko@yahoo.com <rafeyenko@yahoo.com>
2023-03-22 17:01:12 +0100

    23.profileInfo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,17 +3,22 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
-        <div>
-            <div>My posts</div>
-            <textarea></textarea>
-            <button>Add post</button>
-            <button>Remove</button>
-            <div>New posts</div>
-            <div className={classes.masonry}>
+        <div className={classes.masonry}>
+            <h3>New posts</h3>
+            <div>
+                <textarea></textarea>
+            </div>
+            <div>
+                <button>Add post</button>
+            </div>
+            <div>
                 <Post message='hi! hi! hi!' />
-                <Post message='hello world' />
-                <Post message='how are you' />
+                <Post message='hello world!' />
+                <Post message="how are you? let's walk together" />
                 <Post message='have a nice day' />
+                <Post message='finally spring! everything is blossoming!' />
+                <Post message='where are you?' />
+                <Post message='changes! welcome!' />
             </div>
         </div>
     )
